1d111491396478334799da2925830e5d726a38d7
Tatt Chitrakorn <tchitrakorn@dyn-160-39-230-67.dyn.columbia.edu>
2021-10-09 00:38:04 -0400

    Added /events route

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
 const app = express();
 const port = 8080;
+const db = require('./queries.js');
 
 app.use(express.static('public'));
+
+app.get('/events', (req, res) => {
+  db.getEvents()
+    .then(results => res.status(200).send(results))
+    .catch(error => res.status(500).send(error))
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
