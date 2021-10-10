b518668da9ab08a6f784c8b55359b40d27c71ddc
Tatt Chitrakorn <tchitrakorn@dyn-160-39-230-67.dyn.columbia.edu>
2021-10-10 06:11:04 -0400

    Merged homefeed

--- client/components/HomeFeed/HomeFeed.jsx
+++ client/components/HomeFeed/HomeFeed.jsx
@@ -2,26 +2,22 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import FilterPanel from "./FilterPanel.jsx";
 import EventBoard from "./EventBoard.jsx";
-import helpers from "./helpers.js";
+import helpers from './helpers.js';
 
-function HomeFeed() {
+function HomeFeed(props) {
     const [events, setEvents] = useState([]);
     const [inperson, setInperson] = useState(true);
     const [virtual, setVirtual] = useState(true);
-    const [groupSize, setGroupSize] = useState("");
+    const [groupSize, setGroupSize] = useState('');
 
     useEffect(() => {
+        console.log('userId in HomeFeed: ', props.userId);
         axios
             .get("/events")
             .then((response) => {
                 let events = response.data;
-                let filteredEvents = helpers.filterEvents(
-                    events,
-                    inperson,
-                    virtual,
-                    groupSize
-                );
-                console.log("events: ", events);
+                let filteredEvents = helpers.filterEvents(events, inperson, virtual, groupSize);
+                console.log('events: ', events);
                 setEvents(filteredEvents);
             })
             .catch((error) => {
@@ -31,17 +27,16 @@ function HomeFeed() {
 
     return (
         <div class="homefeed">
-            <FilterPanel
+            <FilterPanel 
                 setInperson={setInperson}
                 setVirtual={setVirtual}
                 setGroupSize={setGroupSize}
                 groupSize={groupSize}
                 inperson={inperson}
-                virtual={virtual}
-            />
-            <div class="filter-placeholder">a</div>
-
-            <EventBoard events={events} />
+                virtual={virtual}/>
+            <EventBoard 
+                events={events} 
+                userId={props.userId}/>
         </div>
     );
 }
