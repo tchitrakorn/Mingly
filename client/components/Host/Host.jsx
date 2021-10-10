4b28c7d6d4519a4230787a1e0cc87fa66d9a1104
Faustina Cheng <chengfaustina@gmail.com>
2021-10-10 05:36:35 -0400

    Add design for hosting form

--- client/components/Host/Host.jsx
+++ client/components/Host/Host.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Host(props) {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [location, setLocation] = useState('');
-    const [date, setDate] = useState('');
-    const [time, setTime] = useState('');
-    const [groupSize, setGroupSize] = useState('');
-    const [mode, setMode] = useState('');
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [location, setLocation] = useState("");
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("");
+    const [groupSize, setGroupSize] = useState("");
+    const [mode, setMode] = useState("");
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
@@ -20,28 +20,30 @@ function Host(props) {
             date: date,
             time: time,
             groupSize: groupSize,
-            mode: mode
+            mode: mode,
         };
-        axios.post('/formsubmit', data)
+        axios
+            .post("/formsubmit", data)
             .then((response) => {
                 console.log(response);
-                alert('Form successfully submitted!');
-                setTitle('');
-                setDescription('');
-                setLocation('');
-                setDate('');
-                setTime('');
-                setGroupSize('');
-                setMode('');
+                alert("Form successfully submitted!");
+                setTitle("");
+                setDescription("");
+                setLocation("");
+                setDate("");
+                setTime("");
+                setGroupSize("");
+                setMode("");
             })
             .catch((error) => {
-                console.log('error');
-            })
+                console.log("error");
+            });
     };
 
     return (
-        <div>
+        <div class="form-wrapper">
             <form onSubmit={handleFormSubmit}>
+                <div class="host-title">Host your event</div>
                 <div>
                     <label>
                         Title:
@@ -50,7 +52,8 @@ function Host(props) {
                             id="title"
                             name="title"
                             placeholder="Example: Movie Night"
-                            onChange={e => setTitle(e.target.value)} />
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
                     </label>
                 </div>
                 <div>
@@ -62,63 +65,79 @@ function Host(props) {
                             id="description"
                             name="description"
                             placeholder="Example: Let's go watch Soul!"
-                            onChange={e => setDescription(e.target.value)} />
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
                     </label>
                 </div>
                 <div>
                     <label>
                         Location:
-                        <input 
+                        <input
                             type="text"
                             id="location"
                             name="location"
                             placeholder="Example: Local AMC"
-                            onChange={e => setLocation(e.target.value)} />
+                            onChange={(e) => setLocation(e.target.value)}
+                        />
                     </label>
                 </div>
-                <div>
-                    <label>
-                        Date:
-                        <input 
-                            type="date"
-                            id="date"
-                            name="date"
-                            onChange={e => setDate(e.target.value)} />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        Time:
-                        <input 
-                            type="time"
-                            id="time"
-                            name="time"
-                            onChange={e => setTime(e.target.value)} />
-                    </label>
+                <div class="split">
+                    <div>
+                        <label>
+                            Date:
+                            <input
+                                type="date"
+                                id="date"
+                                name="date"
+                                onChange={(e) => setDate(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Time:
+                            <input
+                                type="time"
+                                id="time"
+                                name="time"
+                                onChange={(e) => setTime(e.target.value)}
+                            />
+                        </label>
+                    </div>
                 </div>
                 <div>
                     <label>
                         Preferred Group Size:
-                        <input 
+                        <input
                             type="text"
                             id="groupSize"
                             name="groupSize"
-                            onChange={e => setGroupSize(e.target.value)} />
+                            onChange={(e) => setGroupSize(e.target.value)}
+                        />
                     </label>
                 </div>
                 <div>
                     <label>
                         Mode of Hangout:
-                        <select id="mode" name="mode" onChange={e => setMode(e.target.value)} >
+                        <select
+                            id="mode"
+                            name="mode"
+                            onChange={(e) => setMode(e.target.value)}
+                        >
                             <option value="in-person">In-person</option>
                             <option value="virtual">Virtual</option>
                         </select>
                     </label>
                 </div>
-                <input className='form-submit' type='submit' value='Submit Form' ></input>
+                <input
+                    id="form-submit"
+                    className="form-submit"
+                    type="submit"
+                    value="Submit Form"
+                ></input>
             </form>
         </div>
-    )
+    );
 }
 
 export default Host;
