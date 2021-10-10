2bc59c9af0002b910c2b7dd37045addf74e39b37
Tatt Chitrakorn <tchitrakorn@dyn-160-39-230-67.dyn.columbia.edu>
2021-10-10 03:39:51 -0400

    Implemented basic form functionality (making request to server)

--- server/index.js
+++ server/index.js
@@ -30,6 +30,24 @@ app.post('/signup', (req, res) => {
     .catch(error => res.status(500).send(error));
 })
 
+app.post('/formsubmit', (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  let location = req.body.location;
+  let date = req.body.date;
+  let time = req.body.time;
+  let groupSize = req.body.groupSize;
+  let mode = req.body.mode;
+  console.log('title: ', title);
+  console.log('description: ', description);
+  console.log('location: ', location);
+  console.log('date: ', date);
+  console.log('time: ', time);
+  console.log('groupSize: ', groupSize);
+  console.log('mode: ', mode);
+  res.status(200).send('done!');
+})
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
