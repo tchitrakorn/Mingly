c951b41daa8c31f71bcc9755d9fe93df5f39938f
Faustina Cheng <chengfaustina@gmail.com>
2021-10-10 00:10:38 -0400

    Add link hover

--- client/components/HomeFeed/FilterPanel.jsx
+++ client/components/HomeFeed/FilterPanel.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function FilterPanel() {
     return (
         <div class="filter-wrapper">
-            <span class="filter-by">Filter By:</span>
-            <span>Time</span>
-            <span>Location</span>
-            <span>Mode</span>
-            <span>Group Size</span>
+            <p class="filter-by">Filter By:</p>
+            <a>Time</a>
+            <a>Location</a>
+            <a>Mode</a>
+            <a>Group Size</a>
         </div>
     );
 }
