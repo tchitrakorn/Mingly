f4a5c6f42e95dc835771e44370f6076e131a83e2
Faustina Cheng <chengfaustina@gmail.com>
2021-10-10 08:06:01 -0400

    Add events page design

--- client/components/MyEvent/MyEvent.jsx
+++ client/components/MyEvent/MyEvent.jsx
@@ -1,56 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import MyEventCard from './MyEventCard.jsx';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MyEventCard from "./MyEventCard.jsx";
 
 function MyEvent(props) {
     const [attendingEvents, setAttendingEvents] = useState([]);
     const [hostingEvents, setHostingEvents] = useState([]);
 
     useEffect(() => {
-        console.log('userId: ', props.userId);
+        console.log("userId: ", props.userId);
         let params = {
-            userId: props.userId
+            userId: props.userId,
         };
 
-        axios.get('/usersEvents', { params })
-        .then((response) => {
-            setAttendingEvents(response.data);
-        })
-        .catch((error) => {
-            console.log('error');
-        })
+        axios
+            .get("/usersEvents", { params })
+            .then((response) => {
+                setAttendingEvents(response.data);
+            })
+            .catch((error) => {
+                console.log("error");
+            });
 
-        axios.get('/hostingEvents', { params })
-        .then((response) => {
-            console.log(response);
-            setHostingEvents(response.data);
-        })
-        .catch((error) => {
-            console.log('error');
-        })
+        axios
+            .get("/hostingEvents", { params })
+            .then((response) => {
+                console.log(response);
+                setHostingEvents(response.data);
+            })
+            .catch((error) => {
+                console.log("error");
+            });
     });
 
     return (
-        <div>
-            <div>
-                My Profile:
-                <div>DEFAULT IMAGE GOES HERE</div>
-                <div>Name: {props.userName}</div>
+        <div class="myevent-wrapper auto-margin">
+            <div class="profile-wrapper">
+                <div>
+                    <img
+                        class="profile-icon"
+                        src="https://i.ibb.co/RbK369F/cute-animals-portrait-face-panda-bear-fox-cat-rabbit-fox-deer-raccon-icons-vector-illustration.jpg"
+                        alt="An adorable bear icon"
+                    />
+                </div>
+                <div class="profile-name">{props.userName}</div>
             </div>
-            <div>
-                Hosting:
-                {hostingEvents.map((event) => (
-                    <MyEventCard event={event} />
-                ))}
-            </div>
-            <div>
-                Attending:
-                {attendingEvents.map((event) => (
-                    <MyEventCard event={event} />
-                ))}
+            <div class="split">
+                <div>
+                    <p class="myevents-title">Hosting</p>
+                    {hostingEvents.map((event) => (
+                        <MyEventCard event={event} />
+                    ))}
+                </div>
+                <div>
+                    <p class="myevents-title">Attending</p>
+                    {attendingEvents.map((event) => (
+                        <MyEventCard event={event} />
+                    ))}
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default MyEvent;
